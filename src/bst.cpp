1057f05b27378a3021f7186761839312813c0215
Tangjp-wraith <3081129260@qq.com>
2022-11-27 23:01:36 +0800

    update

--- src/bst.cpp
+++ src/bst.cpp
@@ -1,1 +1,72 @@
 #include "bst.h"
+
+// BST::Node 
+
+BST::Node::Node(int _value, Node* _left,Node* _right)
+    :value{_value},left{_left},right{_right}{}
+
+BST::Node::Node(const Node& node)
+    :value{node.value},left{node.left},right{node.right}{}
+
+std::ostream& operator<<(std::ostream& os, const BST::Node& node){
+    os << &node << "\t=> value:" << node.value << "\t   ";
+    os << "left:" << std::left;
+    os.width(12);
+    os << node.left << std::right;
+    os.width(12);
+    os << "right:" << std::left;
+    os << node.right;
+    return os;
+}
+
+//BST
+
+BST::BST(std::initializer_list<int> node)
+    :root{nullptr}
+{
+    for(const int* node_iterator_ptr=node.begin){
+        this->add_node();
+    }
+}
+
+BST::BST(const BST& bst)
+    :root{nullptr}
+{       
+
+}   
+
+BST::BST(BST&& bst)
+    :root{}
+{
+
+}
+
+BST::~BST(){
+    std::vector<Node*> nodes;
+    bfs([&nodes](BST::Node*& node){
+        nodes.push_back(node);
+        });
+    for(auto& node:nodes){
+        delete node;
+    }
+}
+
+void BST::bfs(std::function<void(Node*& node)> func)const{
+
+}   
+
+BST::Node*& BST::get_root(){
+    return root;
+}
+
+size_t BST::length() const{
+    size_t len=0;
+    this->bfs([&len](BST::Node*& node){
+        len++;
+    });
+    return len;
+}
+
+bool BST::add_node(int value){
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include <gtest/gtest.h>
 #include "bst.h"
 
+using foo = void(int); // 定义函数类型, using 的使用见上一节中的别名语法
+void functional(foo f) { // 参数列表中定义的函数类型 foo 被视为退化后的函数指针类型 foo*
+    f(1); // 通过函数指针调用函数
+}
+
+
 int main(int argc, char **argv)
 {
     if (false) // make false to run unit-tests
     {
-        // debug section
-
+        auto f = [](int value) {
+            std::cout << value << std::endl;
+        };
+        functional(f); // 传递闭包对象，隐式转换为 foo* 类型的函数指针值
+        f(1); // lambda 表达式调用
     }
     else
     {
